055e073c9612609128b5be4ef55036010d3ab950
Piyush S <singh.piyush575@gmail.com>
2024-06-05 23:01:42 +0530

    Project structure and notebook experiment added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="liver_disease",
+    version="0.0.0",
+    author="Piyush Singh",
+    author_email="singh.piyush575@gmail.com",
+    description="INDIA Liver Disease Prediction Project",
+    packages=find_packages()
+)
